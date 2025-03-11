caebfa1c015ecc4dbe67d39d82afadd981ec7384
Cutaku <mmzz0219@gmail.com>
2025-03-11 14:46:40 +0900

    feat : 코드 정리

--- concertseat/src/main/java/site/concertseat/domain/member/entity/Member.java
+++ concertseat/src/main/java/site/concertseat/domain/member/entity/Member.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
--- concertseat/src/main/java/site/concertseat/domain/review/repository/BatchRepository.java
+++ concertseat/src/main/java/site/concertseat/domain/review/repository/BatchRepository.java
@@ -3,7 +3,6 @@ package site.concertseat.domain.review.repository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import site.concertseat.domain.review.entity.ReviewFeature;
 import site.concertseat.domain.review.entity.ReviewObstruction;
 import site.concertseat.domain.review.entity.Sight;
@@ -16,7 +15,6 @@ import java.util.List;
 public class BatchRepository {
     private final JdbcTemplate jdbcTemplate;
 
-    @Transactional
     public void saveSights(List<Sight> sights){
         String sql = "insert into sight(compressed_image, image, review_id) values (?, ?, ?)";
 
@@ -30,7 +28,6 @@ public class BatchRepository {
                 });
     }
 
-    @Transactional
     public void saveObstructions(List<ReviewObstruction> obstructions){
         String sql = "insert into review_obstruction(review_id, obstruction_id) values (?, ?)";
 
@@ -43,7 +40,6 @@ public class BatchRepository {
                 });
     }
 
-    @Transactional
     public void saveFeatures(List<ReviewFeature> features){
         String sql = "insert into review_feature(review_id, feature_id) values (?, ?)";
 
