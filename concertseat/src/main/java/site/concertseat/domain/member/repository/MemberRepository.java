352e2eb2adcc2385c35c9749c2d722a7a0572953
jeongjiyeon315 <jung_j_yeon@naver.com>
2025-04-08 21:26:45 +0900

    feat : 내 정보, 내 리뷰 및 관심 리뷰 개수 조회 추가

--- concertseat/src/main/java/site/concertseat/domain/member/repository/MemberRepository.java
+++ concertseat/src/main/java/site/concertseat/domain/member/repository/MemberRepository.java
@@ -1,7 +1,9 @@
 package site.concertseat.domain.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import site.concertseat.domain.member.dto.MemberInfo;
 import site.concertseat.domain.member.entity.Member;
 
 import java.util.Optional;
@@ -12,4 +14,12 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUuid(String uuid);
 
     Optional<Member> findBySocialId(String socialId);
+
+    @Query("select new site.concertseat.domain.member.dto.MemberInfo(" +
+            "m.nickname, " +
+            "m.src, " +
+            "m.socialId) " +
+            "from Member m " +
+            "where m.id = :memberId")
+    MemberInfo findMemberInfoByMemberId(Long memberId);
 }
