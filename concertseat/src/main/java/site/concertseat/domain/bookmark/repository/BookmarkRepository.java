7dbc3bde3596c58b9505477fe3983ce4c3a021aa
jeongjiyeon315 <jung_j_yeon@naver.com>
2025-04-12 22:11:07 +0900

    fix : findBookmarkByReviewsAndMemberId 함수 파라미터 변경

--- concertseat/src/main/java/site/concertseat/domain/bookmark/repository/BookmarkRepository.java
+++ concertseat/src/main/java/site/concertseat/domain/bookmark/repository/BookmarkRepository.java
@@ -16,7 +16,7 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, BookmarkId>,
             "from Bookmark b " +
             "where b.review.id in :reviews " +
             "and b.member.id = :memberId")
-    List<Bookmark> findBookmarkByReviewsAndMemberId(@Param("reviews") List<Review> reviews, @Param("memberId") Long memberId);
+    List<Bookmark> findBookmarkByReviewsAndMemberId(@Param("reviews") List<Long> reviews, @Param("memberId") Long memberId);
 
     @Query("select count(b) " +
             "from Bookmark b " +
