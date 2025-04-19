69e5cc49a7d3643874791620fbf62972ec58b1fd
Cutaku <mmzz0219@gmail.com>
2025-04-19 20:37:02 +0900

    test : 북마크 추가 및 해제 테스트 코드 작성

--- concertseat/src/test/java/site/concertseat/domain/review/controller/ReviewControllerTest.java
+++ concertseat/src/test/java/site/concertseat/domain/review/controller/ReviewControllerTest.java
@@ -37,8 +37,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static site.concertseat.global.statuscode.ErrorCode.*;
-import static site.concertseat.global.statuscode.SuccessCode.CREATED;
-import static site.concertseat.global.statuscode.SuccessCode.OK;
+import static site.concertseat.global.statuscode.SuccessCode.*;
 import static site.concertseat.utils.ResponseFieldUtils.getCommonResponseFields;
 
 @Transactional
@@ -1390,6 +1389,188 @@ public class ReviewControllerTest {
                                 )
                                 .requestSchema(Schema.schema("내 후기 상세 조회 Request"))
                                 .responseSchema(Schema.schema("내 후기 상세 조회 Response"))
+                                .build()
+                        ))
+                );
+    }
+
+    @Test
+    public void 북마크_추가_성공() throws Exception {
+        //given
+        Long reviewId = 101L;
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                post("/api/reviews/{reviewId}/bookmarks", reviewId)
+                        .header("Authorization", "Bearer " + jwtToken)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+                        .with(csrf())
+        );
+
+        //then
+        actions
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.header.message").value(CREATED.getMessage()))
+                .andDo(document(
+                        "북마크 추가 성공",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        resource(ResourceSnippetParameters.builder()
+                                .tag("Review API")
+                                .summary("북마크 추가 API")
+                                .requestHeaders(
+                                        headerWithName("Authorization")
+                                                .description("JWT 토큰")
+                                )
+                                .pathParameters(
+                                        parameterWithName("reviewId")
+                                                .description("리뷰 아이디")
+                                )
+                                .responseFields(
+                                        getCommonResponseFields(
+                                                fieldWithPath("body").type(NULL)
+                                                        .description("내용 없음")
+                                        )
+                                )
+                                .requestSchema(Schema.schema("북마크 추가 Request"))
+                                .responseSchema(Schema.schema("북마크 추가 Response"))
+                                .build()
+                        ))
+                );
+    }
+
+    @Test
+    public void 북마크_추가_실패_없는_리뷰_아이디() throws Exception {
+        //given
+        Long reviewId = 10001L;
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                post("/api/reviews/{reviewId}/bookmarks", reviewId)
+                        .header("Authorization", "Bearer " + jwtToken)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+                        .with(csrf())
+        );
+
+        //then
+        actions
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.header.message").value(NOT_FOUND.getMessage()))
+                .andDo(document(
+                        "북마크 추가 실패 - 없는 리뷰 아이디",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        resource(ResourceSnippetParameters.builder()
+                                .tag("Review API")
+                                .summary("북마크 추가 API")
+                                .requestHeaders(
+                                        headerWithName("Authorization")
+                                                .description("JWT 토큰")
+                                )
+                                .pathParameters(
+                                        parameterWithName("reviewId")
+                                                .description("리뷰 아이디")
+                                )
+                                .responseFields(
+                                        getCommonResponseFields(
+                                                fieldWithPath("body").type(NULL)
+                                                        .description("내용 없음")
+                                        )
+                                )
+                                .requestSchema(Schema.schema("북마크 추가 Request"))
+                                .responseSchema(Schema.schema("북마크 추가 Response"))
+                                .build()
+                        ))
+                );
+    }
+
+    @Test
+    public void 북마크_해제_성공() throws Exception {
+        //given
+        Long reviewId = 102L;
+
+        //when
+
+        ResultActions actions = mockMvc.perform(
+                delete("/api/reviews/{reviewId}/bookmarks", reviewId)
+                        .header("Authorization", "Bearer " + jwtToken)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+                        .with(csrf())
+        );
+
+        //then
+        actions
+                .andExpect(status().isNoContent())
+                .andExpect(jsonPath("$.header.message").value(DELETED.getMessage()))
+                .andDo(document(
+                        "북마크 해제 성공",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        resource(ResourceSnippetParameters.builder()
+                                .tag("Review API")
+                                .summary("북마크 해제 API")
+                                .requestHeaders(
+                                        headerWithName("Authorization")
+                                                .description("JWT 토큰")
+                                )
+                                .pathParameters(
+                                        parameterWithName("reviewId")
+                                                .description("리뷰 아이디")
+                                )
+                                .requestSchema(Schema.schema("북마크 해제 Request"))
+                                .build()
+                        ))
+                );
+    }
+
+    @Test
+    public void 좋아요_해제_실패_없는_리뷰_아이디() throws Exception {
+        //given
+        Long reviewId = 10001L;
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                delete("/api/reviews/{reviewId}/bookmarks", reviewId)
+                        .header("Authorization", "Bearer " + jwtToken)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+                        .with(csrf())
+        );
+
+        //then
+        actions
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.header.message").value(NOT_FOUND.getMessage()))
+                .andDo(document(
+                        "북마크 해제 실패 - 없는 리뷰 아이디",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        resource(ResourceSnippetParameters.builder()
+                                .tag("Review API")
+                                .summary("북마크 추가 API")
+                                .requestHeaders(
+                                        headerWithName("Authorization")
+                                                .description("JWT 토큰")
+                                )
+                                .pathParameters(
+                                        parameterWithName("reviewId")
+                                                .description("리뷰 아이디")
+                                )
+                                .responseFields(
+                                        getCommonResponseFields(
+                                                fieldWithPath("body").type(NULL)
+                                                        .description("내용 없음")
+                                        )
+                                )
+                                .requestSchema(Schema.schema("북마크 해제 Request"))
+                                .responseSchema(Schema.schema("북마크 해제 Response"))
                                 .build()
                         ))
                 );
