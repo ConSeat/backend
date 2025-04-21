8d8808235eadc5eba564ed29267dfc233ff73558
Cutaku <mmzz0219@gmail.com>
2025-04-21 23:30:42 +0900

    feat : 리뷰 정렬 기준 변경

--- concertseat/src/test/java/site/concertseat/domain/review/controller/ReviewControllerTest.java
+++ concertseat/src/test/java/site/concertseat/domain/review/controller/ReviewControllerTest.java
@@ -4,7 +4,6 @@ import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.epages.restdocs.apispec.Schema;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -1819,7 +1818,7 @@ public class ReviewControllerTest {
 
         Long lastReviewId = 102L;
 
-        String sort = "createdAt";
+        String sort = "modifiedAt";
 
         ResultActions actions = mockMvc.perform(
                 get("/api/reviews/seating/{seatingId}/list", seatingId)
